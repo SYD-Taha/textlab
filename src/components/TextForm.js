d7619024a0e40a94cef1e6630f0b27f852ade889
SYD-Taha <tjamil00000@gmail.com>
2025-05-01 21:02:29 +0500

    url change

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -46,7 +46,7 @@ export default function TextForm(props) {
 
   const analyzeText = async (action) => {
     try {
-      const response = await fetch(`http://127.0.0.1:5000/${action}`, {
+      const response = await fetch(`https://teejay.pythonanywhere.com/${action}`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ text })
